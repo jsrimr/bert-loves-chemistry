38a749870ffe4a11a441d7c3fa54cff9c526acac
jsrimr <jsrimr@naver.com>
2022-08-23 07:14:00 +0000

    이름 이상한 변수(is_train->eval_mode) 수정

--- data_model.py
+++ data_model.py
@@ -12,13 +12,13 @@ def create_label(Reorg_g, Reorg_ex):
     return torch.tensor([Reorg_g, Reorg_ex], dtype=torch.float)
 
 class ChemiDataset(Dataset):
-    def __init__(self, cfg, df, is_train=False):
+    def __init__(self, cfg, df, eval_mode=False):
         self.cfg = cfg
         # self.tokenizer = tokenizer
         # self.max_seq_length = max_seq_length
-        self.is_train = is_train
+        self.eval_mode = eval_mode
         self.smiles = df['SMILES'].values
-        if not self.is_train:
+        if not self.eval_mode:
             self.Reorg_g = df['Reorg_g'].values
             self.Reorg_ex = df['Reorg_ex'].values
 
@@ -28,6 +28,6 @@ class ChemiDataset(Dataset):
     def __getitem__(self, item):
         inputs = prepare_input(self.cfg.TOKENIZER, self.cfg.MAX_SEQ_LENGTH, self.smiles[item])
         label = np.array([])
-        if not self.is_train:
+        if not self.eval_mode:
             label = create_label(self.Reorg_g[item], self.Reorg_ex[item])
         return inputs, label
